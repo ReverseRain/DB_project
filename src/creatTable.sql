2a8018db1df4162ee36a8f7bcd45553f30e4d435
Hhhhhzz1 <133622974+Hhhhhzz1@users.noreply.github.com>
2023-10-23 17:31:10 +0800

    Update creatTable.sql

--- src/creatTable.sql
+++ src/creatTable.sql
@@ -1,21 +1,21 @@
 create table if not exists user_(
-    mid varchar(30) primary key  ,
+    mid varchar(100) primary key  ,
 --     name 用户名是否可以重复？
 --可以吧
-    name varchar(20) not null ,
-    sex varchar(2),
-    birthday varchar(10),
-    level integer,
-    sign varchar(80),
-    identity varchar(10) not null
+    name varchar(50) not null ,
+    sex varchar(50),
+    birthday varchar(50),
+    level varchar(50),
+    sign varchar(150),
+    identity varchar(30) not null
 );
 create table if not exists following(
     id serial primary key ,
-    user_mid varchar(30),
+    user_mid varchar(100),
     constraint fk1 foreign key (user_mid) references user_(mid)
         on UPDATE cascade
         on DELETE cascade ,
-    following_mid varchar(30),
+    following_mid varchar(100),
     constraint fk2 foreign key (following_mid)references user_(mid)
         on UPDATE cascade
         on DELETE cascade ,
@@ -23,28 +23,29 @@ create table if not exists following(
 );
 
 create table if not exists videos(
-    BV varchar(30) primary key,
-    title varchar(30),
+    BV varchar(100) primary key,
+    title varchar(100),
 --     owner mid 视频所有者是只能有一个人吗？
 --不知
-    owner_mid varchar(30) ,
+    owner_mid varchar(100) ,
     constraint fk1 foreign key (owner_mid) references user_(mid),
-    commit_time varchar(25),
-    review_time varchar(25),
-    public_time varchar(25),
+    owner_name varchar(100),
+    commit_time timestamp(0),
+    review_time timestamp(0),
+    public_time timestamp(0),
     duration integer,
-    description varchar(80),
-    reviewer varchar(30),
+    description varchar(10000),
+    reviewer varchar(100),
     constraint fk2 foreign key (reviewer) references user_(mid)
 );
 
 create table if not exists like_(
     id serial primary key ,
-    BV varchar(30),
-    user_mid varchar(30) ,
-    constraint fk1 foreign key (BV) references videos(BV)
+    BV varchar(100),
+    user_mid varchar(100) ,
+   constraint fk1 foreign key (BV) references videos(BV)
         on DELETE cascade
-        on UPDATE cascade ,
+       on UPDATE cascade ,
     constraint fk2 foreign key (user_mid) references user_(mid)
         on DELETE cascade
         on UPDATE cascade ,
@@ -52,8 +53,8 @@ create table if not exists like_(
 );
 create table if not exists coin(
     id serial primary key ,
-    BV varchar(30),
-    user_mid varchar(30) ,
+    BV varchar(100),
+    user_mid varchar(100) ,
     constraint fk1 foreign key (BV) references videos(BV)
         on DELETE cascade
         on UPDATE cascade ,
@@ -64,8 +65,8 @@ create table if not exists coin(
 );
 create table if not exists favorite(
     id serial primary key ,
-    BV varchar(30),
-    user_mid varchar(30) ,
+    BV varchar(100),
+    user_mid varchar(100) ,
     constraint fk1 foreign key (BV) references videos(BV)
         on DELETE cascade
         on UPDATE cascade ,
@@ -76,8 +77,8 @@ create table if not exists favorite(
 );
 create table if not exists view_time(
     id serial primary key ,
-    BV varchar(10),
-    user_mid varchar(30),
+    BV varchar(100),
+    user_mid varchar(100),
     watch_time integer,
     constraint fk1 foreign key (BV) references videos(BV)
          on UPDATE cascade
@@ -95,8 +96,8 @@ create table if not exists danmu(
     id serial primary key ,
     BV varchar(100),
     mid varchar(100),
-    time varchar(100),
-    content varchar(1000)，
-    constraint fk1 foreign key (BV) references videos(BV),
+    time varchar(20),
+    content varchar(500),
+    --constraint fk1 foreign key (BV) references videos(BV),
     constraint fk2 foreign key (mid) references user_(mid)
 );
